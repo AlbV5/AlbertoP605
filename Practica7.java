918c82b53d9a31b5a3395874eac672058d769d9e
AlbV5 <119131365+AlbV5@users.noreply.github.com>
2024-03-07 20:00:09 -0600

    Add files via upload

---
+++ Practica7.java
@@ -0,0 +1,17 @@
+function validar(){
+    var Usuario=Get Elemtby id("Caja2")Value;
+    var Contra=Get Elemtby id("Caja4")Value;
+    var User="Redes"
+     if(Usuario==""){
+        alert ("El usuario no puede estar vacio");
+     }
+     else if(Contra==""){
+        alert ("La contraseña no puede estar vacia");
+     }
+     else{(Usuario==User and Contra==Pass)
+     locatino("Iniciohtml");
+     }
+     else{
+        alert ("usuario o contrasena incorrecta");
+     }
+}
